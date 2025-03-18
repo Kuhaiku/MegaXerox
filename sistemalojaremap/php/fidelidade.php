40a34c9098eaa8689232bc3de83caeaeb0482e5f
Leonardo Raposo <82468796+Kuhaiku@users.noreply.github.com>
2025-03-18 13:53:45 -0300

    Create fidelidade.php

---
+++ sistemalojaremap/php/fidelidade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu - Programa de Fidelidade</title>
+</head>
+<body>
+    <h1>Menu de Navegação</h1>
+    <ul>
+        <li><a href="cadastrar_cliente.php">Cadastrar Cliente</a></li>
+        <li><a href="cadastrar_venda.php">Cadastrar Venda</a></li>
+        <li><a href="listar_clientes.php">Listar Clientes</a></li>
+        <li><a href="listar_vendas.php">Listar Vendas</a></li>
+    </ul>
+</body>
+</html>
