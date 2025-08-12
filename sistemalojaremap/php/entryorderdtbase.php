20b8a12e8451b433e0bed3929a1b7b1b06d5e079
Leonardo Raposo <82468796+Kuhaiku@users.noreply.github.com>
2025-08-12 14:15:49 -0300

    Update entryorderdtbase.php

--- sistemalojaremap/php/entryorderdtbase.php
+++ sistemalojaremap/php/entryorderdtbase.php
@@ -78,7 +78,7 @@ if (mysqli_num_rows($result) > 0) {
         echo "<li><a href='generate_exitorder.php?$queryString' class='generate-btn2'>Gerar Ordem de Saída</a></li>";
         
         echo "</ul>";
-        echo "</ul>";
+        
         echo "</div>";
     }
     echo "</div>";
@@ -108,6 +108,7 @@ document.getElementById('search').addEventListener('input', function() {
 
 </body>
 </html>
+
 
 
 
